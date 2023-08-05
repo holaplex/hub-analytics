cc4da7e0f8c4f2da4850782157a1d77707ac8f67
mpw <x@mpw.sh>
2023-08-04 22:19:54 -0300

    add migrations

---
+++ migration/src/m20230804_212412_create_organizations_table.rs
@@ -0,0 +1,43 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(Organizations::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(Organizations::Id)
+                            .uuid()
+                            .not_null()
+                            .primary_key(),
+                    )
+                    .col(
+                        ColumnDef::new(Organizations::Name)
+                            .string()
+                            .not_null()
+                            .unique_key(),
+                    )
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(Organizations::Table).to_owned())
+            .await
+    }
+}
+
+#[derive(Iden)]
+pub enum Organizations {
+    Table,
+    Id,
+    Name,
+}
---
+++ migration/src/m20230804_214701_create_mints_table.rs
@@ -0,0 +1,67 @@
+use sea_orm_migration::prelude::*;
+
+use crate::m20230804_212530_create_projects_table::Projects;
+use crate::m20230804_213809_create_collections_table::Collections;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(Collections::Table)
+                    .if_not_exists()
+                    .col(ColumnDef::new(Mints::Id).uuid().not_null().primary_key())
+                    .col(ColumnDef::new(Mints::CollectionId).uuid().not_null())
+                    .foreign_key(
+                        ForeignKey::create()
+                            .name("fk-mints_collection_id-collections")
+                            .from(Mints::Table, Mints::CollectionId)
+                            .to(Collections::Table, Collections::Id)
+                            .on_delete(ForeignKeyAction::Cascade)
+                            .on_update(ForeignKeyAction::Cascade),
+                    )
+                    .col(ColumnDef::new(Mints::ProjectId).uuid().not_null())
+                    .foreign_key(
+                        ForeignKey::create()
+                            .name("fk-mints_project_id-projects")
+                            .from(Mints::Table, Mints::ProjectId)
+                            .to(Projects::Table, Projects::Id)
+                            .on_delete(ForeignKeyAction::Cascade)
+                            .on_update(ForeignKeyAction::Cascade),
+                    )
+                    .col(ColumnDef::new(Mints::Timestamp).timestamp().not_null())
+                    .to_owned(),
+            )
+            .await?;
+
+        manager
+            .create_index(
+                IndexCreateStatement::new()
+                    .name("mints_collection_id_idx")
+                    .table(Collections::Table)
+                    .col(Collections::ProjectId)
+                    .index_type(IndexType::Hash)
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(Collections::Table).to_owned())
+            .await
+    }
+}
+
+#[derive(Iden)]
+enum Mints {
+    Table,
+    Id,
+    CollectionId,
+    ProjectId,
+    Timestamp,
+}
